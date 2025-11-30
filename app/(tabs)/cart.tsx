61bfcb6c52b6ca3afed095d09a8acc9a266c5a1a
Jendosek <bala01081977@gmail.com>
2025-11-30 23:01:40 +0200

    cart page initial commit

--- app/(tabs)/cart.tsx
+++ app/(tabs)/cart.tsx
@@ -1,112 +1,192 @@
-import { Image } from 'expo-image';
-import { Platform, StyleSheet } from 'react-native';
+import React, { useState } from "react";
+import { View, Text, StyleSheet, TouchableOpacity, FlatList, Image } from "react-native";
+import { colors } from '../components/Colors';
+import { Ionicons } from "@expo/vector-icons";
+import { useOrderStore } from "../store/index";
+import { SafeAreaView } from "react-native-safe-area-context";
 
-import { Collapsible } from '@/components/ui/collapsible';
-import { ExternalLink } from '@/components/external-link';
-import ParallaxScrollView from '@/components/parallax-scroll-view';
-import { ThemedText } from '@/components/themed-text';
-import { ThemedView } from '@/components/themed-view';
-import { IconSymbol } from '@/components/ui/icon-symbol';
-import { Fonts } from '@/constants/theme';
+const BasketScreen = () => {
+    const orders = useOrderStore((state) => state.orders);
+    const clearOrders = useOrderStore((state) => state.clearOrders);
+    const removeOrder = useOrderStore((state) => state.removeOrder);
+    const confirmOrder = useOrderStore((state) => state.confirmOrder);
+    const increaseQuantity = useOrderStore((state) => state.increaseQuantity);
+    const decreaseQuantity = useOrderStore((state) => state.decreaseQuantity);
+    const totalItems = useOrderStore((state) => state.totalItems);
+    const getPriceForPizza = useOrderStore((state) => state.getPriceForPizza);
+    const [confirmationMessage, setConfirmationMessage] = useState('');
 
-export default function TabTwoScreen() {
-  return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#D0D0D0', dark: '#353636' }}
-      headerImage={
-        <IconSymbol
-          size={310}
-          color="#808080"
-          name="chevron.left.forwardslash.chevron.right"
-          style={styles.headerImage}
-        />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText
-          type="title"
-          style={{
-            fontFamily: Fonts.rounded,
-          }}>
-          Explore
-        </ThemedText>
-      </ThemedView>
-      <ThemedText>This app includes example code to help you get started.</ThemedText>
-      <Collapsible title="File-based routing">
-        <ThemedText>
-          This app has two screens:{' '}
-          <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> and{' '}
-          <ThemedText type="defaultSemiBold">app/(tabs)/explore.tsx</ThemedText>
-        </ThemedText>
-        <ThemedText>
-          The layout file in <ThemedText type="defaultSemiBold">app/(tabs)/_layout.tsx</ThemedText>{' '}
-          sets up the tab navigator.
-        </ThemedText>
-        <ExternalLink href="https://docs.expo.dev/router/introduction">
-          <ThemedText type="link">Learn more</ThemedText>
-        </ExternalLink>
-      </Collapsible>
-      <Collapsible title="Android, iOS, and web support">
-        <ThemedText>
-          You can open this project on Android, iOS, and the web. To open the web version, press{' '}
-          <ThemedText type="defaultSemiBold">w</ThemedText> in the terminal running this project.
-        </ThemedText>
-      </Collapsible>
-      <Collapsible title="Images">
-        <ThemedText>
-          For static images, you can use the <ThemedText type="defaultSemiBold">@2x</ThemedText> and{' '}
-          <ThemedText type="defaultSemiBold">@3x</ThemedText> suffixes to provide files for
-          different screen densities
-        </ThemedText>
-        <Image
-          source={require('@/assets/images/react-logo.png')}
-          style={{ width: 100, height: 100, alignSelf: 'center' }}
-        />
-        <ExternalLink href="https://reactnative.dev/docs/images">
-          <ThemedText type="link">Learn more</ThemedText>
-        </ExternalLink>
-      </Collapsible>
-      <Collapsible title="Light and dark mode components">
-        <ThemedText>
-          This template has light and dark mode support. The{' '}
-          <ThemedText type="defaultSemiBold">useColorScheme()</ThemedText> hook lets you inspect
-          what the user&apos;s current color scheme is, and so you can adjust UI colors accordingly.
-        </ThemedText>
-        <ExternalLink href="https://docs.expo.dev/develop/user-interface/color-themes/">
-          <ThemedText type="link">Learn more</ThemedText>
-        </ExternalLink>
-      </Collapsible>
-      <Collapsible title="Animations">
-        <ThemedText>
-          This template includes an example of an animated component. The{' '}
-          <ThemedText type="defaultSemiBold">components/HelloWave.tsx</ThemedText> component uses
-          the powerful{' '}
-          <ThemedText type="defaultSemiBold" style={{ fontFamily: Fonts.mono }}>
-            react-native-reanimated
-          </ThemedText>{' '}
-          library to create a waving hand animation.
-        </ThemedText>
-        {Platform.select({
-          ios: (
-            <ThemedText>
-              The <ThemedText type="defaultSemiBold">components/ParallaxScrollView.tsx</ThemedText>{' '}
-              component provides a parallax effect for the header image.
-            </ThemedText>
-          ),
-        })}
-      </Collapsible>
-    </ParallaxScrollView>
-  );
-}
+    const total = calculateTotalAmount(orders, getPriceForPizza);
+
+    function calculateTotalAmount(orders: any[], getPriceForPizza: (item: any) => { pricePizza: string; price: number; volume: string }) {
+        let totalAmount = 0;
+        orders.forEach((item) => {
+            const priceInfo = getPriceForPizza(item);
+            const itemTotal = priceInfo.price * (item.quantity || 1);
+            totalAmount += itemTotal;
+        });
+        return {
+            totalAmount: `$${totalAmount.toFixed(2)}`,
+        };
+    }
+
+    const renderItem = ({ item }) => (
+        <View style={styles.item}>
+            <View style={styles.imageContainer}>
+                <Image source={item.image} style={styles.image} />
+            </View>
+            <View style={styles.wrapRight}>
+                <View style={styles.wrapTitle}>
+                    <TouchableOpacity>
+                        <Ionicons name="close-circle" size={20} color={colors.title} />
+                    </TouchableOpacity>
+                </View>
+                <View style={styles.priceContainer}>
+                    <View style={styles.priceText}>
+                        <Text style={styles.titlePrice}>Price:</Text>
+                        <Text style={styles.price}>{getPriceForPizza(item).pricePizza}</Text>
+                    </View>
+                </View>
+                <View style={styles.quantityContainer}>
+                    <TouchableOpacity onPress={() => increaseQuantity(item)}>
+                        <Text style={styles.quantityButton}>+</Text>
+                    </TouchableOpacity>
+                    <Text style={styles.quantity}>{item.quantity}</Text>
+                    <TouchableOpacity onPress={() => decreaseQuantity(item)}>
+                        <Text style={styles.quantityButton}>-</Text>
+                    </TouchableOpacity>
+                </View>
+            </View>
+        </View>
+    )
+
+    return (
+        <SafeAreaView style={styles.container}>
+            <View style={styles.titleContainer}>
+                <Text style={styles.titleClear}>Clear</Text>
+                <Text style={styles.titleOrder}>{`In your basket ${totalItems} items`}</Text>
+            </View>
+            <FlatList
+                data={orders}
+                renderItem={renderItem}
+                keyExtractor={(item) => `${item.id} - ${item.selectedSize}`} />
+            <View style={styles.cartItemContainer}>
+                {orders.length === 0 && (
+                    <Image source={require('../../assets/images/homeScreen/icon-basket.png')} style={styles.iconCart} />
+                )}
+            </View>
+            {orders.length > 0 && (
+                <View style={styles.totalContainer}>
+                    <View style={styles.wrapTotal}>
+                        <View style={styles.wrapTotalTitle}>
+                            <Text style={styles.totalTitle}>Total:</Text>
+                            <Text style={styles.totalPrice}>{total.totalAmount}</Text>
+                        </View>
+                    </View>
+                    <TouchableOpacity style={styles.confirmButton}>
+                        <Text style={styles.wrapTotal}>Confirm Order</Text>
+                    </TouchableOpacity>
+                </View>
+            )}
+        </SafeAreaView>
+    );
+};
+
+
 
 const styles = StyleSheet.create({
-  headerImage: {
-    color: '#808080',
-    bottom: -90,
-    left: -35,
-    position: 'absolute',
-  },
-  titleContainer: {
-    flexDirection: 'row',
-    gap: 8,
-  },
+    container: {
+        flex: 1,
+        backgroundColor: colors.mainBackground,
+    },
+    titleContainer: {
+        marginHorizontal: 30,
+        marginTop: 10,
+    },
+    titleClear: {
+        fontSize: 18,
+        marginLeft: 'auto',
+        color: colors.green,
+    },
+    titleOrder: {
+        fontSize: 18,
+        fontWeight: 'bold',
+        color: colors.textColor,
+    },
+    item: {
+        backgroundColor: colors.itemBackground,
+        margin: 10,
+        padding: 5,
+        gap: 20,
+        minHeight: 100,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        borderRadius: 10,
+        borderWidth: 2,
+        borderColor: colors.shadowColor,
+    },
+    imageContainer: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        padding: 8,
+    },
+    image: {
+        width: 120,
+        height: 120,
+        resizeMode: 'stretch'
+    },
+    wrapRight: {
+        gap: 10,
+        flex: 1,
+    },
+    wrapTitle: {
+        flexDirection: 'row',
+        alignItems: 'flex-end',
+    },
+    priceContainer: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+    },
+    priceText: {
+        flexDirection: 'column',
+    },
+    titlePrice: {
+        fontSize: 14,
+        fontWeight: 'bold',
+        textDecorationLine: 'underline',
+        color: colors.textColor,
+    },
+    price: {
+        fontSize: 20,
+        color: colors.green,
+    },
+    quantityContainer: {
+        flexDirection: 'row',
+        marginTop: 20,
+        borderWidth: 1,
+        borderColor: colors.green,
+        borderRadius: 10,
+    },
+    quantityButton: {
+        marginHorizontal: 20,
+        fontSize: 16,
+        fontWeight: 'bold',
+        color: colors.green,
+    },
+    quantity: {
+        fontSize: 18,
+        color: colors.white,
+    },
+    cartItemContainer: {
+        flex: 70,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    iconCart: {
+        width: 91,
+        height: 91,
+    }
+
 });
+
+export default BasketScreen;
